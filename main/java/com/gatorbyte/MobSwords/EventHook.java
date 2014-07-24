7158c69654843a8ab562f9cfae3313090bc0b596
kblackstock <blackstockkevin@gmail.com>
2014-07-23 22:09:05 -0500

    Removing unnecessary commented code.

--- main/java/com/gatorbyte/MobSwords/EventHook.java
+++ main/java/com/gatorbyte/MobSwords/EventHook.java
@@ -27,17 +27,6 @@ public class EventHook
 				entity.setLocationAndAngles(event.entity.posX, event.entity.posY + 1, event.entity.posZ, MathHelper.wrapAngleTo180_float(world.rand.nextFloat() * 360.0F), 0.0F);
 				world.spawnEntityInWorld(entity);
 			}
-//			else
-//			{
-//				//particle can only be spawned on the client
-//				Random rand = new Random();
-//				for(int i = 0; i <= 20; ++i)
-//				{
-//					world.spawnParticle("reddust", event.entity.posX + (rand.nextDouble() - 0.5D) * (double)event.entity.width, event.entity.posY + rand.nextDouble() * (double)event.entity.height - (double)event.entity.yOffset, event.entity.posZ + (rand.nextDouble() - 0.5D) * (double)event.entity.width, 0.0D, 0.0D, 0.0D);
-//				}	
-//			}
 		}
-		
-
 	}
 }
